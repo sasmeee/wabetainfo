d8bd5638d0884e516a11b4e02e7420f4a7a94882
Sasmitha Ashinsana <89065560+sasmeee@users.noreply.github.com>
2023-08-17 01:40:06 +0530

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-const { waBetaHome, getLatestPost } = require("./wabetainfo");
-
-// to get all posts in home page
-
-waBetaHome().then(console.log);
-
-// to get leatest post in home page
-
-getLatestPost().then(console.log);
